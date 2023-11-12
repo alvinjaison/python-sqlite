7f9de7da6656be56b0ccb54f3e001975422c0364
Alvin Jaison <alvinjaison@outlook.com>
2023-11-12 12:53:42 +0000

    Update school.py

--- school.py
+++ school.py
@@ -10,18 +10,6 @@ cursor = conn.cursor()
 
 # Create a table
 cursor.execute('''
-    CREATE TABLE student (
-        id INTEGER PRIMARY KEY,
-        name_nominal_column TEXT,
-        gender_ordinal_column TEXT,
-        percentage_interval_column REAL,
-        year_ratio_column INTEGER,
-        scholarship_foreign_key_column INTEGER,
-        compound_key_column      
-    )
-''')
-
-cursor.execute('''
     CREATE TABLE course (
         course_id INTEGER PRIMARY KEY,
         course_name TEXT,
@@ -30,6 +18,19 @@ cursor.execute('''
         difficulty TEXT,
         fee INTEGER,
         duration INTEGER   
+    )
+''')
+
+cursor.execute('''
+    CREATE TABLE student (
+        id INTEGER PRIMARY KEY,
+        name_nominal_column TEXT,
+        gender_ordinal_column TEXT,
+        percentage_interval_column REAL,
+        year_ratio_column INTEGER,
+        compound_key_column,
+        course_id INTEGER,
+        FOREIGN KEY (course_id) REFERENCES course(course_id)
     )
 ''')
 
@@ -42,9 +43,9 @@ for _ in range(1000):
     gender_ordinal_data = random.choice(['Male', 'Female'])
     percentage_interval_data = random.uniform(50.0, 100.0)
     year_ratio_data = random.randint(1990, 2000)
-    scholarship_foreign_key_data = random.randint(10000, 20000)
     compound_key_data = ''.join(random.choice(string.ascii_uppercase) for _ in range(10))
-    cursor.execute("INSERT INTO student (name_nominal_column, gender_ordinal_column, percentage_interval_column, year_ratio_column, scholarship_foreign_key_column, compound_key_column) VALUES (?, ?, ?, ?, ?, ?)", (name_nominal_data, gender_ordinal_data, percentage_interval_data, year_ratio_data, scholarship_foreign_key_data, compound_key_data))
+    course_id_data = random.randint(1, 1000)
+    cursor.execute("INSERT INTO student (name_nominal_column, gender_ordinal_column, percentage_interval_column, year_ratio_column, compound_key_column, course_id) VALUES (?, ?, ?, ?, ?, ?)", (name_nominal_data, gender_ordinal_data, percentage_interval_data, year_ratio_data, compound_key_data, course_id_data))
 
 
 # Generate and insert random data into the table
